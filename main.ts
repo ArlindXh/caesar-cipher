f852a963ed9ce6933b6a81d8ea5de822e6095992
ArlindXh <xhakoliarlind@gmail.com>
2020-01-02 04:09:22 +0100

    add support for upper case characters

--- main.ts
+++ main.ts
@@ -1,43 +1,44 @@
 export class Cipher {
-  private alphabet: Array<string> = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
+  private alphabet: Array<Array<string>> = [
+    ["a", "A"],
+    ["b", "B"],
+    ["c", "C"],
+    ["d", "D"],
+    ["e", "E"],
+    ["f", "F"],
+    ["g", "G"],
+    ["h", "H"],
+    ["i", "I"],
+    ["j", "J"],
+    ["k", "K"],
+    ["l", "L"],
+    ["m", "M"],
+    ["n", "N"],
+    ["o", "O"],
+    ["p", "P"],
+    ["q", "Q"],
+    ["r", "R"],
+    ["s", "S"],
+    ["t", "T"],
+    ["u", "U"],
+    ["v", "V"],
+    ["w", "W"],
+    ["x", "X"],
+    ["y", "Y"],
+    ["z", "Z"],
   ];
-
   private getCharacterPositions(text: String) {
     let newString: String = text.replace(/\s+/g, "+");
     let characters: Array<string | number> = newString.split("");
-    let characterPositions: Array<string | number> = [];
+    let characterPositions: Array<Object | string> = [];
     characters.map((character) => {
       this.alphabet.map((c, i) => {
         if (character === "+" && characterPositions[characterPositions.length - 1] !== "+") {
           characterPositions = [...characterPositions, character];
-        } else if (c === character) {
-          characterPositions = [...characterPositions, i];
+        } else if (character === c[0]) {
+          characterPositions = [...characterPositions, { position: i, index: 0 }];
+        } else if (character === c[1]) {
+          characterPositions = [...characterPositions, { position: i, index: 1 }];
         }
       });
     });
@@ -48,25 +49,25 @@ export class Cipher {
     let remainderLength: number;
     let newString = "";
     if (positiveShift) {
-      charPositions.map((pos) => {
-        if (pos === "+") {
+      charPositions.map((char) => {
+        if (char === "+") {
           newString = newString + " ";
-        } else if (pos + shiftLength > this.alphabet.length - 1) {
-          remainderLength = pos + shiftLength - this.alphabet.length;
-          newString = newString + this.alphabet[remainderLength];
+        } else if (char.position + shiftLength > this.alphabet.length - 1) {
+          remainderLength = char.position + shiftLength - this.alphabet.length;
+          newString = newString + this.alphabet[remainderLength][char.index];
         } else {
-          newString = newString + this.alphabet[pos + shiftLength];
+          newString = newString + this.alphabet[char.position + shiftLength][char.index];
         }
       });
     } else {
-      charPositions.map((pos) => {
-        if (pos === "+") {
+      charPositions.map((char) => {
+        if (char === "+") {
           newString = newString + " ";
-        } else if (pos - shiftLength < 0) {
-          remainderLength = pos - shiftLength + this.alphabet.length;
-          newString = newString + this.alphabet[remainderLength];
+        } else if (char.position - shiftLength < 0) {
+          remainderLength = char.position - shiftLength + this.alphabet.length;
+          newString = newString + this.alphabet[remainderLength][char.index];
         } else {
-          newString = newString + this.alphabet[pos - shiftLength];
+          newString = newString + this.alphabet[char.position - shiftLength][char.index];
         }
       });
     }
